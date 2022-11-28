f37ce41c5da0f3a4185d5dccb1582f3e915d5f8a
kanade-ec <iseekana@gmail.com>
2022-11-29 06:12:16 +0900

    型注釈を使うようにリファクタリング

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -1,30 +1,151 @@
 module Picshare exposing (main)
+
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+
+
+
+-- START:model.alias
+
+
+type alias Model =
+    { url : String
+    , caption : String
+    , liked : Bool
+    }
+
+
+
+-- END:model.alias
+
 
 baseUrl : String
 baseUrl =
-  "https://programming-elm.com/"
+    "https://programming-elm.com/"
 
-initialModel : { url : String, caption : String }
+
+
+-- START:initialModel.annotation
+
+
+initialModel : Model
+
+
+
+-- END:initialModel.annotation
+
+
 initialModel =
-    { url = baseUrl ++ "1.jpg", caption = "Surfing" }
+    { url = baseUrl ++ "1.jpg"
+    , caption = "Surfing"
+    , liked = False
+    }
 
-viewDetailedPhoto: { url : String, caption : String } -> Html msg
+
+
+-- START:viewDetailedPhoto.annotation
+
+
+viewDetailedPhoto : Model -> Html Msg
+
+
+
+-- END:viewDetailedPhoto.annotation
+
+
 viewDetailedPhoto model =
-  div [ class "detailed-photo" ]
-      [ img [ src model.url ] []
-      , div [ class "photo-info" ]
-        [ h2 [ class "caption" ] [ text model.caption ] ]
-      ]
-view : { url : String, caption : String } -> Html msg
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+
+            else
+                Like
+    in
+    div [ class "detailed-photo" ]
+        [ img [ src model.url ] []
+        , div [ class "photo-info" ]
+            [ div [ class "like-button" ]
+                [ i
+                    [ class "fa fa-2x"
+                    , class buttonClass
+                    , onClick msg
+                    ]
+                    []
+                ]
+            , h2 [ class "caption" ] [ text model.caption ]
+            ]
+        ]
+
+
+
+-- START:view.annotation
+
+
+view : Model -> Html Msg
+
+
+
+-- END:view.annotation
+
+
 view model =
     div []
         [ div [ class "header" ]
-          [ h1 [] [ text "Picshare" ] ]
+            [ h1 [] [ text "Picshare" ] ]
         , div [ class "content-flow" ]
-          [ viewDetailedPhoto model ]
+            [ viewDetailedPhoto model ]
         ]
 
-main : Html msg
-main = view initialModel
+
+type Msg
+    = Like
+    | Unlike
+
+
+
+-- START:update.annotation
+
+
+update : Msg -> Model -> Model
+
+
+
+-- END:update.annotation
+
+
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+
+-- START:main.annotation
+
+
+main : Program () Model Msg
+
+
+
+-- END:main.annotation
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
